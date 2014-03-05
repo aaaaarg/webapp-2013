2b59073fd753bb1159881f2aa536192221f703a0
sdockray <sean.patrick.dockray@gmail.com>
2014-03-05 16:43:03 +1100

    admin changes

--- flask_application/controllers/admin.py
+++ flask_application/controllers/admin.py
@@ -51,7 +51,7 @@ class ThingView(ModelView):
 
 class CollectionView(ModelView):
 	column_filters = ['title']
-	column_searchable_list = ('title')
+	column_searchable_list = ('title',)
 	column_list = ('title')
 	form_excluded_columns = ('creator','followers','editors')
 
