e6f863981e88fded125fb1e96b9de3ef34326807
sdockray <sean.patrick.dockray@gmail.com>
2014-06-20 18:07:03 +1000

    script for removing redundant uploads

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ sys.path.insert(0, os.getcwd())
 from flask_application import app
 from flask.ext.script import Manager, Server
 
-from flask_application.script import ResetDB, PopulateDB, SolrReindex, MigrateMakers, MigrateUsers, MigrateInvitations, MigrateCollections, MigrateFollowers, MigrateComments, MigrateQueues, MigrateFiles, ProcessFiles, ProcessUploads, FixShortDescriptions
+from flask_application.script import ResetDB, PopulateDB, SolrReindex, FixMD5s
 
 from flask.ext.security.script import (CreateUserCommand , AddRoleCommand,
         RemoveRoleCommand, ActivateUserCommand, DeactivateUserCommand)
@@ -20,17 +20,7 @@ manager.add_command("populate_db", PopulateDB())
 
 manager.add_command("solr_reindex", SolrReindex())
 
-manager.add_command("migrate_makers", MigrateMakers())
-manager.add_command("migrate_users", MigrateUsers())
-manager.add_command("migrate_invitations", MigrateInvitations())
-manager.add_command("migrate_collections", MigrateCollections())
-manager.add_command("migrate_followers", MigrateFollowers())
-manager.add_command("migrate_comments", MigrateComments())
-manager.add_command("migrate_queues", MigrateQueues())
-manager.add_command("migrate_files", MigrateFiles())
-manager.add_command("process_files", ProcessFiles())
-manager.add_command("process_uploads", ProcessUploads())
-manager.add_command("fix_short_descriptions", FixShortDescriptions())
+manager.add_command("fix_md5s", FixMD5s())
 
 manager.add_command('create_user', CreateUserCommand())
 manager.add_command('add_role', AddRoleCommand())
