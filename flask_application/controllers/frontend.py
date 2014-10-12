732066e74c1707d80619331387852b291597b689
sdockray <sean.patrick.dockray@gmail.com>
2014-10-12 13:51:40 +1100

    trying to improve phrase queries

--- flask_application/controllers/frontend.py
+++ flask_application/controllers/frontend.py
@@ -145,9 +145,11 @@ def research(type=False):
 		#results = solr.query(content_type="page", text=query).paginate(start=start, rows=num).highlight("searchable_text", snippets=3, maxAnalyzedChars=-1).execute()
 		query_tokens = query.split()
 		combined = ' '.join(query_tokens)
-		new_query = '"%s"~%d' % (combined, len(query_tokens))
+		new_query = "'%s'~%d" % (combined, 0)
 		#new_query = "'%s'" % query
 		the_query = solr.query(searchable_text=new_query).filter(content_type="page").sort_by("-score").paginate(start=start, rows=num)
+		# Hack to exclude the card catalogue, which could very well overrun search results
+		the_query.filter_exclude(md5_s="7dbf4aee8eb2b19197fe62913e15dda5")
 		results = the_query.execute()
 		# Build list of results 
 		things = []
