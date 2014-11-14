f662024878985c3e9bdbfcf7c22d4d432474b0a8
sdockray <sean.patrick.dockray@gmail.com>
2014-11-14 18:33:13 +1100

    update clips to new pdfserver

--- flask_application/static/js/figleaf.js
+++ flask_application/static/js/figleaf.js
@@ -240,7 +240,7 @@
             var $div = this.page_containers[p];
             if($div.children.length == 0) {
                 var $img = document.createElement("img");
-                $img.src = this.basepath + "x1024-" + p + ".jpg";
+                $img.src = this.basepath + "x" + SCANR.page_h + "-" + p + ".jpg";
                 $img.style.width = "700";
                 $div.appendChild($img);
             }
