85cb8b63d4b2c3fc5c97578f8e4cc1bf9df58b72
sdockray <sean.patrick.dockray@gmail.com>
2014-12-02 10:41:13 +1100

    generate a pdf from a search

--- flask_application/controllers/compiler.py
+++ flask_application/controllers/compiler.py
@@ -13,26 +13,60 @@ compiler = Blueprint('compiler', __name__, url_prefix='/compiler')
 
 @compiler.route('/create', methods= ['GET', 'POST'])
 def create():
-	user_id = current_user.get_id()
-	annotations = Reference.objects.filter(creator=user_id).order_by('-created_at')
-	if not annotations:
-		return create_from_recent()
-	clips = build_clips(annotations)
+	m = request.args.get('mode', None)
+	v = request.args.get('value', None)
 
+	if not m and not v:
+		user_id = current_user.get_id()
+		annotations = Reference.objects.filter(creator=user_id).order_by('-created_at')
+		# if there are no annotations this way, then just fetch recent ones
+		if not annotations:
+			annotations = Reference.objects(pos_end__gt=0).order_by('-created_at').limit(20)
+	elif m=='tag' and v is not None:
+		annotations = Reference.objects.filter(tags=v).order_by('-created_at')
+	elif m=='recent':
+		annotations = Reference.objects(pos_end__gt=0).order_by('-created_at').limit(20)
+	
+	clips = build_clips(annotations)
 	return render_template('compiler/create.html',
 		title = "compiler",
 		clips = clips
 	)
 
-@compiler.route('/create/recent', methods= ['GET', 'POST'])
-def create_from_recent():
-	annotations = Reference.objects(pos_end__gt=0).order_by('-created_at').limit(20)
-	clips = build_clips(annotations)
 
-	return render_template('compiler/create.html',
-		title = "compiler",
-		clips = clips
-	)
+@compiler.route('/create/from/search', methods= ['GET', 'POST'])
+def create_from_search():
+	query = request.args.get('query', "")
+	num = 100
+	start = 0
+	content = ""
+
+	if not query=="":
+		#results = solr.query(content_type="page", text=query).paginate(start=start, rows=num).highlight("searchable_text", snippets=3, maxAnalyzedChars=-1).execute()
+		#query_tokens = query.split()
+		#combined = 'AND '.join(query_tokens)
+		#new_query = "'%s'~%d" % (combined, len(query_tokens))
+		new_query = "%s" % query
+		the_query = solr.query(searchable_text=new_query).filter(content_type="page").filter_exclude(md5_s="7dbf4aee8eb2b19197fe62913e15dda5").sort_by("-score").paginate(start=start, rows=num)
+		results = the_query.execute()
+		# Build list of results 
+		pdf_path = ''
+		last_md5 = None
+		for result in results:
+			if '_id' in result:
+				# id[0] is the upload id, id[1] is upload page
+				md5 = result['md5_s']
+				id = str(result['_id']).split('_')
+				if len(id)==2:
+					if md5==last_md5:
+						pdf_path = '%s,%s' % (pdf_path, id[1])
+					else:
+						pdf_path = '/%s/%s' % (pdf_path, id[1])
+	
+	if pdf_path=='':
+		return 'There were no results!'
+	else:
+		return url_for('reference.preview', filename='/compiler%s' % pdf_path)
 
 
 def build_clips(annotations):
