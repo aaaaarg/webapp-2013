fd120c5cc2e8a743b5ec9a711f6779c3680653b6
sdockray <sean.patrick.dockray@gmail.com>
2015-01-10 12:29:39 +1100

    encoding error on moving file

--- flask_application/models/upload.py
+++ flask_application/models/upload.py
@@ -161,7 +161,7 @@ class Upload(SolrMixin, CreatorMixin, db.Document):
 		"""
 		def safe_name(str):
 			#return "".join([c for c in str if c.isalpha() or c.isdigit() or c==' ']).rstrip()[:64]
-			return "".join([c for c in str if c.isalpha() or c.isdigit() or c==' ']).rstrip()
+			return "".join([c for c in str if c.isalpha() or c.isdigit() or c==' ']).rstrip().encode('utf-8')
 
 		def splitpath(path, maxdepth=20):
 			( head, tail ) = os.path.split(path)
