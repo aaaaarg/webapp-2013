50f26015a63dd7eb663ff72300f516a59ca5e160
sdockray <sean.patrick.dockray@gmail.com>
2015-01-29 16:54:26 +1100

    trying to fix unicode problem

--- flask_application/models/upload.py
+++ flask_application/models/upload.py
@@ -162,7 +162,7 @@ class Upload(SolrMixin, CreatorMixin, db.Document):
 		def safe_name(str):
 			#return "".join([c for c in str if c.isalpha() or c.isdigit() or c==' ']).rstrip()[:64]
 			s = "".join([c for c in str if c.isalpha() or c.isdigit() or c==' ']).rstrip()
-			return unicodedata.normalize('NFKD', s).encode('ascii','ignore')
+			return unicodedata.normalize('NFKD', unicode(s)).encode('ascii','ignore')
 
 		def splitpath(path, maxdepth=20):
 			( head, tail ) = os.path.split(path)
